5a95f749deb0d9244361f6eef9f6e853682cb10e
jplulu <lujason899@gmail.com>
2019-10-13 16:55:43 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/StackandQueueV2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StackandQueueV2.dir/main.cpp.o"
+  "StackandQueueV2.pdb"
+  "StackandQueueV2.exe"
+  "libStackandQueueV2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StackandQueueV2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
